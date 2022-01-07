45c5cbdf4834d1a1c3e18f70fd3beac01c804834
Michael Ndon <manj19.mjn@gmail.com>
2022-01-07 10:33:32 +0100

    Added models and starter files
    Added models and starter files

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const port = 8080;
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+
+
 const app = express();
 
+//Middleware processing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.raw());
+app.use(bodyParser.json());
 
-app.listen(4000)
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.listen(port, function () {
+    console.log(`Node server is running... http://localhost:${port}`);
+  });
